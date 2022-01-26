d26f0776ff6650b3e5f76cc4f55461447b3d6eff
Shawn Poulson <spoulson@pathwire.com>
2022-01-26 17:28:21 -0500

    Tidy tests.

--- tracing/tracing_live_test.go
+++ tracing/tracing_live_test.go
@@ -21,11 +21,10 @@ func TestTracing(t *testing.T) {
 	defer func() {
 		err := tracing.CloseTracing(ctx)
 		require.NoError(t, err)
-		time.Sleep(2 * time.Second)
 	}()
 
 	t.Run("Simple traces", func(t *testing.T) {
-		ctx, span := tracer.Start(ctx, "Send traces")
+		ctx, span := tracer.Start(ctx, t.Name())
 		defer span.End()
 
 		for i := 0; i < 10; i++ {
@@ -38,7 +37,7 @@ func TestTracing(t *testing.T) {
 	})
 
 	t.Run("Log to trace", func(t *testing.T) {
-		ctx, span := tracer.Start(ctx, "Log to trace")
+		ctx, span := tracer.Start(ctx, t.Name())
 		defer span.End()
 
 		logrus.WithContext(ctx).
@@ -49,7 +48,7 @@ func TestTracing(t *testing.T) {
 
 	t.Run("Scope()", func(t *testing.T) {
 		t.Run("Simple traces", func(t *testing.T) {
-			err := tracing.Scope(ctx, "Scope", func(s *tracing.S) error {
+			err := tracing.Scope(ctx, t.Name(), func(s *tracing.S) error {
 				for i := 0; i < 10; i++ {
 					err := tracing.Scope(s.Ctx, "Span", func(s *tracing.S) error {
 						time.Sleep(10 * time.Millisecond)
@@ -66,7 +65,7 @@ func TestTracing(t *testing.T) {
 		})
 
 		t.Run("Return error", func(t *testing.T) {
-			err := tracing.Scope(ctx, "Error scope", func(s *tracing.S) error {
+			err := tracing.Scope(ctx, t.Name(), func(s *tracing.S) error {
 				time.Sleep(1 * time.Millisecond)
 				return errors.New("Test error")
 			})
