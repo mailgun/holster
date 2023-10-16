c43544ad63780e259a33a76e3d6a4db5bd12b568
Shawn Poulson <spoulson@mailgun.com>
2023-10-16 13:55:05 -0400

    Patch for CVE-2023-45142.

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -15,7 +15,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 
 	"github.com/mailgun/holster/v4/errors"
